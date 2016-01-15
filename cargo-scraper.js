98f53d1c1570a84ae9ac1f5705003240c72a238b
Jeremias Diaz <monitz87@gmail.com>
2016-01-15 17:56:06 -0300

    10000

--- cargo-scraper.js
+++ cargo-scraper.js
@@ -6,7 +6,7 @@ _ = require('lodash');
 Gurkha = require('gurkha');
 Q = require('q');
 SCHEMAS = require('./cargo-scraper.schemas');
-MAX_PAGES = 30000;
+MAX_PAGES = 10000;
 
 function composeUrl (host, path) {
   if (_.contains(path, 'http')) {
