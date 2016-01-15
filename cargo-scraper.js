15dff0a641a8b5217e3991d2869ac260336abb22
Jeremias Diaz <monitz87@gmail.com>
2016-01-15 17:45:18 -0300

    asdf

--- cargo-scraper.js
+++ cargo-scraper.js
@@ -6,7 +6,7 @@ _ = require('lodash');
 Gurkha = require('gurkha');
 Q = require('q');
 SCHEMAS = require('./cargo-scraper.schemas');
-MAX_PAGES = 1000000;
+MAX_PAGES = 100000;
 
 function composeUrl (host, path) {
   if (_.contains(path, 'http')) {
@@ -33,136 +33,137 @@ getMailsTask.builder(function (job) {
 });
 
 getMailsTask.main(function (task, http, params) {
-  var optsTemplate, opts, count, gettingPages;
+  var optsTemplate, opts, count, gettingPages, page;
 
-  count = 0;
-  gettingPages = Q.resolve([]);
+  function handleResponse (result) {
+    var body, linksParser, links, gettingMails;
 
-  for(var page = 0; page < MAX_PAGES; ++page) {
-    function handleResponse (result) {
-      var body, linksParser, links, gettingMails;
+    if (page === 0) {
+      body = result.body;
+    } else {
+      body = JSON.parse(result.body).html;
+    }
+    // console.log(body);
 
-      if (page === 0) {
-        body = result.body;
-      } else {
-        body = JSON.parse(result.body).html;
-      }
+    linksParser = new Gurkha(SCHEMAS.links);
 
-      // console.log(body);
+    links = linksParser.parse(body);
 
-      linksParser = new Gurkha(SCHEMAS.links);
-
-      links = linksParser.parse(body);
-
-      if (!links) {
-        task.success('mails retrieved');
-        return;
-      }
-
-      // console.log(links);
-
-      gettingMails = [];
-
-      _.each(links, function (link) {
-        var opts, request;
-
-        opts = optsTemplate.build({
-          'url': link
-        });
-
-        request = http.get(opts)
-
-        .then(function (result) {
-          var body, opts, mail, linkParser, links, about;
-
-          body = result.body;
-
-          try {
-            mail = body.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i);
-          } catch (e) {
-            return;
-          }
-
-          if (mail === null) {
-            linkParser = new Gurkha(SCHEMAS.internalLinks);
-            links = linkParser.parse(body);
-            // console.log(links);
-            about = _.find(links, function (element) {
-              var lc = element.toLowerCase();
-              return _.contains(lc, 'about') || _.contains(lc, 'info') || _.contains(lc, 'contact');
-            });
-
-            // console.log(about);
-
-            if (about) {
-              opts = optsTemplate.build({
-                'url': composeUrl(result.res.req._headers.host, about)
-              });
-
-              return http.get(opts)
-
-              .then(function (result) {
-                var body, mail;
-
-                body = result.body;
-
-                try {
-                  mail = body.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i);
-                } catch (e) {
-                  return;
-                }
-
-                return mail[0];
-              });
-            }
-          } else {
-            return mail[0];
-          }
-        })
-
-        .then(function (result) {
-          var opts, mail;
-
-          if (result) {
-            mail = result;
-
-            opts = optsTemplate.build({
-              'url': 'http://52.33.130.163//users',
-              'data': {
-                'user_data': {
-                  'user': {
-                    'email': result,
-                    'name': ''
-                  },
-                },
-                'country_code': 21
-              }
-            });
-
-            return http.post(opts)
-
-            .then(function (result) {
-              console.log('____________');
-              console.log(++count);
-              console.log('mail:' + mail);
-              console.log(result.body);
-              console.log('____________');
-            });
-          }
-        });
-
-        gettingMails.push(request);
-      });
-
-      Q.all(gettingMails)
-
-      .then(function (result) {
-      })
-
-      .fail(function (reason) {
-      }).done();
+    if (!links) {
+      task.success('mails retrieved');
+      return;
     }
 
+    // console.log(links);
+
+    gettingMails = [];
+
+    _.each(links, function (link) {
+      var opts, request;
+
+      opts = optsTemplate.build({
+        'url': link
+      });
+
+      request = http.get(opts)
+
+      .then(function (result) {
+        var body, opts, mail, linkParser, links, about;
+
+        body = result.body;
+
+        try {
+          mail = body.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i);
+        } catch (e) {
+          return;
+        }
+
+        if (mail === null) {
+          linkParser = new Gurkha(SCHEMAS.internalLinks);
+          links = linkParser.parse(body);
+          // console.log(links);
+          about = _.find(links, function (element) {
+            var lc = element.toLowerCase();
+            return _.contains(lc, 'about') || _.contains(lc, 'info') || _.contains(lc, 'contact');
+          });
+
+          // console.log(about);
+
+          if (about) {
+            opts = optsTemplate.build({
+              'url': composeUrl(result.res.req._headers.host, about)
+            });
+
+            return http.get(opts)
+
+            .then(function (result) {
+              var body, mail;
+
+              body = result.body;
+
+              try {
+                mail = body.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i);
+              } catch (e) {
+                return;
+              }
+
+              return mail[0];
+            });
+          }
+        } else {
+          return mail[0];
+        }
+      })
+
+      .then(function (result) {
+        var opts, mail;
+
+        if (result) {
+          mail = result;
+
+          opts = optsTemplate.build({
+            'url': 'http://52.33.130.163//users',
+            'data': {
+              'user_data': {
+                'user': {
+                  'email': result,
+                  'name': ''
+                },
+              },
+              'country_code': 21
+            }
+          });
+
+          return http.post(opts)
+
+          .then(function (result) {
+            console.log('____________');
+            console.log(++count);
+            console.log('mail:' + mail);
+            console.log(result.body);
+            console.log('____________');
+          });
+        }
+      });
+
+      gettingMails.push(request);
+    });
+
+    Q.all(gettingMails)
+
+    .then(function (result) {
+    })
+
+    .fail(function (reason) {
+    }).done();
+  }
+
+  count = 0;
+  page = 0;
+  gettingPages = Q.resolve([]);
+
+  for(page = 0; page < MAX_PAGES; ++page) {
+    console.log(page);
     var optsTemplate, opts;
 
     optsTemplate = http.optionsTemplate();
